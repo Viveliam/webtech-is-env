04171e790d1b0ec5f7e91e77ee41349c57e9d281
Levi Gerrits <levigerrits@gmail.com>
2020-12-08 10:54:24 +0100

    Update index.php

--- applicatie/public/index.php
+++ applicatie/public/index.php
@@ -1,17 +1,45 @@
 <?php
-  /*
-  Hier wordt voorbereidend werk gedaan.
-  De resultaten kunnen dan vervolgens met eenvoudige stukken PHP-broncode in de HTML geïnjecteerd worden.
-  Zo krijgen we zo veel mogelijk scheiding van PHP en HTML-broncode.
-  (Zo veel mogelijk scheiding van functionaliteit en presentatie.)
-  */
+  $campert = <<<EOD
+  Verzet begint niet met grote woorden
+  maar met kleine daden
+
+  zoals storm met zacht geritsel in de tuin
+  of de kat die de kolder in zijn kop krijgt
+
+  zoals brede rivieren
+  met een kleine bron
+  verscholen in het woud
+
+  zoals een vuurzee
+  met dezelfde lucifer
+  die een sigaret aansteekt
+
+  zoals liefde met een blik
+  een aanraking iets wat je opvalt in een stem
+
+  jezelf een vraag stellen
+  daarmee begint verzet
+
+  en dan die vraag aan een ander stellen.
+  EOD;  
 
   // We declareren en initialiseren een aantal variabelen.
   $naam = 'Hogeschool van Arnhem en Nijmegen';
-  $adres = 'Ruitenberglaan 31';
-  $postcode = '6826 CC';
   $plaats = 'Arnhem';
+  $adres = 'Ruitenberglaan 31';   //string
+  $postcode = '6826 CC';          //string
+  $geslacht = 'm';                //char
+  $isStudent = 'False';           //boolean
+  $leeftijd = '20';               //int
   $html = "$naam<br>$adres<br>$postcode $plaats";
+  function printOutStringAndLength($input) {
+    echo "<h1>".$input."</h1>";
+    echo "<em>Deze string is ".strlen($input)." tekens lang</em>";
+  }
+  function last3Characters($string) {
+    echo substr($string, strlen($string)-3, 3);
+  }
+  
 ?> 
 <!DOCTYPE html>
 <html lang="nl">
@@ -27,5 +55,8 @@
       <?php echo $html?>
     </address>
     <p><strong>Gebruik de ‘Developer Tools’ om vast te stellen dat alle PHP-broncode omgezet is naar HTML!</strong></p>
+    <p><?php echo "meneer x is $leeftijd jaars oud"?></p>
+    <p><?php printOutStringAndLength($campert)?></p>
+    <p><?php last3Characters($naam)?></p>
   </body>
 </html>
