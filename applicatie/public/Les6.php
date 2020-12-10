00d256b922747112748692e17a434bf94c2789a1
Levi Gerrits (Laptop) <Levigerrits@gmail.com>
2020-12-10 14:25:57 +0100

    ding

---
+++ applicatie/public/Les6.php
@@ -0,0 +1,32 @@
+<?php
+    $huis = [
+        'slaapkamer' => [0, 0, 1, 0, 1, 0],
+        'woonkamer' => [1, 0, 0, 1, 0, 0],
+        'keuken' => [0, 0, 0, 1, 0, 0]  
+    ];
+    function isArrayLongerThan($inputArray, $comparison) {
+        if (count($inputArray) > $comparison) {
+            return true;
+        }
+        else return false;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Voorbereiding Les 6</h1>
+    <h1>Les 6</h1>
+    
+    <h1>Opdracht 1<h1>
+    <p><?php
+    echo isArrayLongerThan($huis["slaapkamer"], 7);
+    echo isArrayLongerThan($huis["woonkamer"], 6);
+    echo isArrayLongerThan($huis["keuken"], 5);
+    ?></p>
+</body>
+</html>
--- applicatie/public/functies.php
+++ applicatie/public/functies.php
@@ -1,19 +1,8 @@
 <?php
 function minutenNaarUur($minuten) {
-    $minuten %= 60; 
-    $uur /= 60;
-    return $uur;
+    $minutenOver = $minuten % 60; 
+    $uur = intval($minuten / 60);
+    $string = $uur.":".$minutenOver;
+    return $string;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
