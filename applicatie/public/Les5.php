a572cdaa9e83062c835af4faf49c3daeffb53af5
Levi Gerrits (Laptop) <Levigerrits@gmail.com>
2020-12-10 13:02:34 +0100

    a

--- applicatie/public/Les5.php
+++ applicatie/public/Les5.php
@@ -1,6 +1,37 @@
 <?php
-
-
+    $array1 = [1,2,3,4,5,6,7,8,9,10];
+    $array2 = array(6,7,8,9,10,11,12,13,14,15);
+    $array3 = array_merge($array1, $array2);
+    $arrayFilms = [
+        [
+          "title" => "A Wrinkle In Time",
+          "year" => 2018,
+          "director" => "Ava DuVernay",
+          "stars" => ["Storm Reid", "Oprah Winfrey"],
+          "imageURL" => "https://m.media-amazon.com/images/M/MV5BMjMxNjQ5MTI3MV5BMl5BanBnXkFtZTgwMjQ2MTAyNDM@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        ],
+        [
+          "title" => "Black Panther",
+          "year" => 2018,
+          "director" => "Ryan Coogler",
+          "stars" => ["Chadwick Boseman", "Michael B. Jordan"],
+          "imageURL" => "https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        ],
+        [
+          "title" => "Tenet",
+          "year" => 2020,
+          "director" => "Christopher Nolan",
+          "stars" => ["John David Washington", "Elizabeth Decki"],
+          "imageURL" => "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_UX182_CR0,0,182,268_AL_.jpg"
+        ],
+        [
+          "title" => "Us",
+          "year" => 2018,
+          "director" => "Jordan Peele",
+          "stars" => ["Lupita Nyong'o ", "Winston Duke"],
+          "imageURL" => "https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        ]
+    ];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +41,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Voorbereiding Les 4</h1>
+    <h1>Voorbereiding Les 5</h1>
+
 </body>
 </html>
---
+++ applicatie/public/functies.php
@@ -0,0 +1,19 @@
+<?php
+function minutenNaarUur($minuten) {
+    $minuten %= 60; 
+    $uur /= 60;
+    return $uur;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
